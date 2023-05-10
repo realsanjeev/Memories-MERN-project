e17f62443f646f94761633486b9c1d447cca3920
RealSanjeev <45820805+realsanjeev@users.noreply.github.com>
2023-05-10 17:21:21 +0000

    Update render method

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import { useDispatch } from "react-redux";
 
 import useStyles from './style';
 
-import Posts from '/workspaces/Mongodb-project/client/src/components/Posts/Posts.js';
-import Form from '/workspaces/Mongodb-project/client/src/components/Form/Form'
+import Posts from './components/Posts/Posts.js';
+import Form from './components/Form/Form'
+import getPosts from './actions/posts'
 
-import memories from '/workspaces/Mongodb-project/client/src/images/memories.jpg';
+import memories from './images/memories.jpg';
 
 const App = () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch])
     return (
         <Container maxidth='lg'>
             <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography variant="h2" align="center">
                     Hello
                 </Typography>
-                <img className={classes.img} src={memories} alt="memories" height='60' />
+                <img className={classes.img} src={memories} alt="memories" height='60px' />
             </AppBar>
             <Grow in>
                 <Container>
@@ -26,7 +33,6 @@ const App = () => {
                             <Posts />
                         </Grid>
                         <Grid item xs={12} sm={7}>
-
                             <Form />
                         </Grid>
                     </Grid>
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createstore, applyMiddleware, compose} from 'redux';
+import thunk from "redux-thunk";
+
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode>
+    <App />
+</React.StrictMode>)
